2927829098f2db4b07c84bb24d2c8e12fe784a0c
Dawid Nogacz <sloenthran@gmail.com>
2019-04-05 10:39:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-"# HLTVDownloader" 
+# Configuration
+
+1. Open config.php
+2. Add your server according to the following "template"
+3. add opening the link every specified time in crontab:
+https://YourPathToScript/index.php?page=crontab
--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-    $server[0]['host'] = '137.74.1.13';
-    $server[0]['user'] = 'hltv_44';
-    $server[0]['pass'] = 'a82agzoy';
-    $server[0]['directory'] = 'cstrike/';
-    $server[0]['prefix'] = 'testy';
-    $server[0]['name'] = 'Testowy serwer';
+    $server[0]['host'] = '127.0.0.1';
+    $server[0]['user'] = 'root';
+    $server[0]['pass'] = 'pass';
+    $server[0]['directory'] = 'cstrike/'; // Directory in FTP with .dem files
+    $server[0]['prefix'] = 'test'; // Enter anything but it must be unique!
+    $server[0]['name'] = 'Test server'; // Server name in menu
