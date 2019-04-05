1e183eb22f5bc88708bc004ae3d3e102b1886895
Dawid Nogacz <sloenthran@gmail.com>
2019-04-05 10:52:45 +0200

    Final commit

--- model/crontab.php
+++ model/crontab.php
@@ -3,24 +3,33 @@
     foreach($server as $key) {
         $ftp = new FTP($key['host'], $key['user'], $key['pass']);
 
-        $fileList = $ftp->getFilesInDirectory($key['directory'], '*.dem');
-        natcasesort($fileList);
+        $filesList = $ftp->getFilesInDirectory($key['directory'], '*.dem');
+        natcasesort($filesList);
 
-        $count = count($fileList);
+        $count = count($filesList);
 
         if($count > 1) {
-            foreach($fileList as $file) {
+            foreach($filesList as $file) {
                 if($count > 1) {
                     $fileName = str_replace("{$key['directory']}hltv-", '', $file);
                     $ftp->downloadFile("{$key['prefix']}_{$fileName}", 'cache/', $file);
                     ZIP::archive("{$key['prefix']}_{$fileName}");
+                    $ftp->removeFile($file);
                 }
 
                 $count--;
             }
         }
 
-        $fileList = File::getFileList("./files");
+        $files = File::getFilesInSpecificPrefix("./files", $key['prefix'], "_");
 
+        foreach($files as $file) {
+            $data = explode("-", $file);
 
+            $date = DateTime::createFromFormat("ymdHi", str_replace("{$key['prefix']}_", "", $data[0]));
+
+            if($date->format("ymd") <= Date::getDateBefore("7 day", "ymd")) {
+                unlink("./files/{$file}.zip");
+            }
+        }
     }
