81b116ef6edc5c7d8c1bc4aebcfe2dcdefc02d27
Dawid Nogacz <sloenthran@gmail.com>
2019-05-31 10:15:08 +0200

    Update Date.php

--- application/Date.php
+++ application/Date.php
@@ -3,6 +3,6 @@
     class Date {
         static function getDateBefore(string $before, string $format) : string {
             $date = date($format);
-            return date($format, strtotime("{$date} -{$before}"));
+            return date($format, strtotime("{$date} - {$before}"));
         }
     }
